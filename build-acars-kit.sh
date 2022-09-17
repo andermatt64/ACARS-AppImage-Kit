825bc18fcad225b4a9770684f4e210de6d2ebd46
andermatt64 <andermatt64@outlook.com>
2022-09-17 13:58:06 -0400

    Add deb builder

--- build-acars-kit.sh
+++ build-acars-kit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BUILDER_TAG_NAME=acars-appimage-builder
+BUILDER_TAG_NAME=acars-appimage-builder-deb
 
-docker build -f builder/Dockerfile . -t  ${BUILDER_TAG_NAME} && \
+docker build -f deb-builder/Dockerfile . -t  ${BUILDER_TAG_NAME} && \
   [ ! -d "$(pwd)/export" ] && mkdir $(pwd)/export && \
   docker run -it --rm -v $(pwd)/export:/export:Z ${BUILDER_TAG_NAME} bash /entrypoint.sh && \
   docker rmi ${BUILDER_TAG_NAME}
