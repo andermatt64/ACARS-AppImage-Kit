c41434ec4596785ce63e5723667a5576ae09bab0
andermatt64 <andermatt64@outlook.com>
2022-09-17 02:30:05 -0400

    Add JAERO and SDRReceiver

--- build-acars-kit.sh
+++ build-acars-kit.sh
@@ -2,7 +2,7 @@
 
 BUILDER_TAG_NAME=acars-appimage-builder
 
-docker build -f builder/Dockerfile . -t  ${BUILDER_TAG_NAME}
-[ ! -d "$(pwd)/export" ] && mkdir $(pwd)/export
-docker run -it --rm -v $(pwd)/export:/export:Z ${BUILDER_TAG_NAME} bash /entrypoint.sh
-docker rmi ${BUILDER_TAG_NAME}
+docker build -f builder/Dockerfile . -t  ${BUILDER_TAG_NAME} && \
+  [ ! -d "$(pwd)/export" ] && mkdir $(pwd)/export && \
+  docker run -it --rm -v $(pwd)/export:/export:Z ${BUILDER_TAG_NAME} bash /entrypoint.sh && \
+  docker rmi ${BUILDER_TAG_NAME}
