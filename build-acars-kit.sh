33ef568fd5a5fbb01ff09effbd4fb248cec81575
andermatt64 <andermatt64@outlook.com>
2022-09-17 18:50:56 -0400

    Add new stuff

--- build-acars-kit.sh
+++ build-acars-kit.sh
@@ -2,8 +2,9 @@
 
 BUILDER_TAG_NAME=acars-appimage-builder-deb
 
-[ -d "$(pwd)/export" ] && rm -rf $(pwd)/export && \
-  docker build -f deb-builder/Dockerfile . -t  ${BUILDER_TAG_NAME} && \
+[ -d "$(pwd)/export" ] && rm -rf $(pwd)/export
+
+docker build -f deb-builder/Dockerfile . -t  ${BUILDER_TAG_NAME} && \
   mkdir $(pwd)/export && \
-  docker run -it --rm -v $(pwd)/export:/export:Z ${BUILDER_TAG_NAME} bash /entrypoint.sh
-  # docker rmi ${BUILDER_TAG_NAME}
+  docker run -it --rm -v $(pwd)/export:/export:Z ${BUILDER_TAG_NAME} bash /entrypoint.sh && \
+  [ "${KEEP_IMG}" != "1" ] && docker rmi ${BUILDER_TAG_NAME}
