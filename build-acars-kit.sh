b2cd9c53b77189ddd40735ce339d5489b8952853
andermatt64 <andermatt64@outlook.com>
2022-09-17 17:49:00 -0400

    Use custom SDR++

--- build-acars-kit.sh
+++ build-acars-kit.sh
@@ -2,7 +2,8 @@
 
 BUILDER_TAG_NAME=acars-appimage-builder-deb
 
-docker build -f deb-builder/Dockerfile . -t  ${BUILDER_TAG_NAME} && \
-  [ ! -d "$(pwd)/export" ] && mkdir $(pwd)/export && \
-  docker run -it --rm -v $(pwd)/export:/export:Z ${BUILDER_TAG_NAME} bash /entrypoint.sh && \
-  docker rmi ${BUILDER_TAG_NAME}
+[ -d "$(pwd)/export" ] && rm -rf $(pwd)/export && \
+  docker build -f deb-builder/Dockerfile . -t  ${BUILDER_TAG_NAME} && \
+  mkdir $(pwd)/export && \
+  docker run -it --rm -v $(pwd)/export:/export:Z ${BUILDER_TAG_NAME} bash /entrypoint.sh
+  # docker rmi ${BUILDER_TAG_NAME}
